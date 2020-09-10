5f5621eb60065a32a4f56a7c527856bf53b2ecc7
Frits Sasia <fritssasia@gmail.com>
2020-09-11 00:19:46 +0800

    minify resource and refactoring code

--- js/preloader.js
+++
@@ -1,6 +0,0 @@
-document.onreadystatechange = function () {
-    if (document.readyState === "complete") {
-        console.log(document.readyState);
-        document.getElementById("PreLoaderBar").style.display = "none";
-    }
-}
